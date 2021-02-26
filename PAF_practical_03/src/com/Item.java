9bb5731af63bff7d1a7eaea93654c6892e287d09
Piumi <aluviharepiumee@gmail.com>
2021-02-26 12:07:04 +0530

    update items

--- PAF_practical_03/src/com/Item.java
+++ PAF_practical_03/src/com/Item.java
@@ -111,32 +111,45 @@ public String readItems()
 	 
 	 // iterate through the rows in the result set
 	 while (rs.next()) 
+		 
 	 { 
 	 String itemID = Integer.toString(rs.getInt("itemID")); 
 	 String itemCode = rs.getString("itemCode"); 
 	 String itemName = rs.getString("itemName"); 
 	 String itemPrice = Double.toString(rs.getDouble("itemPrice")); 
 	 String itemDesc = rs.getString("itemDesc"); 
+	 
+	 
+	 
 	 // Add a row into the html table
 	 output += "<tr><td>" + itemCode + "</td>"; 
 	 output += "<td>" + itemName + "</td>"; 
 	 output += "<td>" + itemPrice + "</td>";
-	 
 	 output += "<td>" + itemDesc + "</td>";
+	 
+	 
+	 
+	 
 	 // buttons
 	 output += "<td><input name='btnUpdate' " 
-	 + " type='button' value='Update'></td>"
+	 + " type='button' value='Update' onclick=\"UpdateItems()\"></td>"
 	 + "<td><form method='post' action='items.jsp'>"
 	 + "<input name='btnRemove' " 
-	 + " type='submit' value='Remove'>"
+	 + " type='submit' value='Remove' onclick=\"deleteItems()\">"
 	 + "<input name='itemID' type='hidden' " 
 	 + " value='" + itemID + "'>" + "</form></td></tr>"; 
 	 } 
+	 
+	 
 	 con.close(); 
 	 // Complete the html table
+	 
+	 
 	 output += "</table>"; 
 	 } 
-	catch (Exception e) 
+	 
+	 
+	 catch (Exception e) 
 	 { 
 		 output = "Error while reading the items."; 
 		 System.err.println(e.getMessage()); 
@@ -147,10 +160,79 @@ public String readItems()
 }
 
 
+public String UpdateItems(String itemID,String code, String name, String price, String desc)
+{
+	
+	 String output = "";
+	 
+	 
+		try
+		 { 
+		 Connection con = connect(); 
+		 if (con == null) 
+		 { 
+		 return "Error while connecting to the database"; 
+		 } 
+		 // create a prepared statement
+		 String query = " Update item set itemCode='"+code+"',itemName = '"+name+"',ItemPrice = '"+price+"',itemDesc = '"+desc+"'"
+				 + "where itemID ='"+itemID+"'";
+		 PreparedStatement preparedStmt = con.prepareStatement(query); 
+		
+		 
+		 
+		 
+		//execute the statement
+		 preparedStmt.execute(); 
+		 con.close(); 
+		 output = "updated successfully"; 
+		 } 
+		catch (Exception e) 
+		 { 
+		 output = "Error while updating the items"; 
+		 System.err.println(e.getMessage()); 
+		 } 
+		return output; 
+	
+}
 
-
+	
+public  boolean deleteItems(String itemID) {
+	
+	int convertedID = Integer.parseInt(itemID);
+	
+	boolean isSuccess = false;
+	try {
+		
+		 Connection con = connect();  
+		 
+		 Statement stmt = (Statement) con.createStatement(); 
+	
+		
+		String sql = "delete from item where itemID = '"+convertedID+"'";
+		int r = stmt.executeUpdate(sql);
+		
+		if(r>0) {
+			
+			isSuccess = true;
+		}
+		else {
+			
+			isSuccess = false;
+		}
+		
+		
+	}catch(Exception e) {
+		
+		e.printStackTrace();
+	}
+	
+	
+	return isSuccess;
+}
 
 
 
 }
+
+
 	
